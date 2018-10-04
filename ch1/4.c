2715c18fca6ae14e5fec97184ae02ff963c516e1
codelegant <laichuanfeng@hotmail.com>
2018-10-04 22:43:38 +0800

    UPDATED 百万级不重复数组生成 v1

--- ch1/4.c
+++ ch1/4.c
@@ -2,29 +2,58 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+// [min, max)
+const size_t kDivideConut = 2500;
+const size_t kLength = 7000000;
+const unsigned kMin = 1000000;
+const unsigned kMax = 10000000;
+const size_t kUnit = kLength / kDivideConut;
+const size_t kUnitReminder = kLength % kDivideConut;
+const unsigned kInterval = (kMax - kMin) / kDivideConut;
+const char *kFileName = "rand.txt";
 
 bool IsUnique(const unsigned *arr, const size_t length, const unsigned target);
-void MakeRandArray(unsigned *arr, const size_t length, const unsigned min, const unsigned max);
-void MakeFileByRandArray(const unsigned *arr, const size_t length, const char *file_name);
+void MakeRandArray(unsigned *arr, const size_t length, const unsigned min, const unsigned max, FILE *fp);
+void DividArray(void);
 
 int main(void)
 {
+  // const unsigned kStart = time(NULL);
+  DividArray();
+  // printf("Time %us\n", (unsigned)time(NULL) - kStart);
+  return 0;
+}
+
+void DividArray(void)
+{
   // int 32位长
   // 7位十进制正整数，使用 unsgined
-  // [min, max] 闭区间
-  const size_t kLength = 1000000;
-  const unsigned kMin = 1000000;
-  const unsigned kMax = 9999999;
-  unsigned arr[kLength];
-  MakeRandArray(arr, kLength, kMin, kMax);
-  MakeFileByRandArray(arr, kLength, "rand_int.txt");
-  return 0;
+  // TODO 够简短高效吗？
+  FILE *fp;
+  if (!(fp = fopen(kFileName, "w")))
+  {
+    fprintf(stderr, "Failed to open file %s\n", kFileName);
+    exit(EXIT_FAILURE);
+  }
+  for (size_t i = 0; i < kDivideConut; i++)
+  {
+    bool last_index = i + 1 == kDivideConut;
+    size_t length = last_index ? kUnit + kUnitReminder : kUnit;
+    unsigned min = kMin + i * kInterval;
+    unsigned max = last_index ? kMax : min + kInterval;
+    unsigned arr[length];
+
+    MakeRandArray(arr, length, min, max, fp);
+    fprintf(stdout, "Processing %zu/%zu\r", i, kDivideConut);
+  }
+
+  if (fclose(fp))
+    fprintf(stderr, "Failed to close %s\n", kFileName);
 }
 
 bool IsUnique(const unsigned *arr, const size_t length, const unsigned target)
 {
   bool result = true;
-  // TODO 需要提升效率
   for (size_t i = 0; i < length; i++)
   {
     if (arr[i] == target)
@@ -36,31 +65,16 @@ bool IsUnique(const unsigned *arr, const size_t length, const unsigned target)
   return result;
 }
 
-void MakeRandArray(unsigned *arr, const size_t length, const unsigned min, const unsigned max)
+void MakeRandArray(unsigned *arr, const size_t length, const unsigned min, const unsigned max, FILE *fp)
 {
   srand((unsigned)time(NULL));
   for (size_t i = 0; i < length; i++)
   {
     unsigned _rand = (unsigned)rand() % max;
     while (_rand < min || !IsUnique(arr, i, _rand))
-      _rand = (unsigned)rand() % max + 1;
+      _rand = (unsigned)rand() % max;
 
     arr[i] = _rand;
+    fprintf(fp, "%u\n", _rand);
   }
-}
-
-void MakeFileByRandArray(const unsigned *arr, const size_t length, const char *file_name)
-{
-  FILE *fp;
-  if (!(fp = fopen(file_name, "w")))
-  {
-    fprintf(stderr, "Failed to open file %s\n", file_name);
-    exit(EXIT_FAILURE);
-  }
-
-  for (size_t i = 0; i < length; i++)
-    fprintf(fp, "%u\n", arr[i]);
-
-  if (fclose(fp))
-    fprintf(stderr, "Failed to close %s\n", file_name);
 }
